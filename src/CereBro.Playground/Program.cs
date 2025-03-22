e3783b04e33485864d9c4e71f8f3ff74b5b66371
rob1997 <robelgetnetrg@gmail.com>
2025-03-23 02:21:22 +0300

    Playground Added

---
+++ src/CereBro.Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using CereBro.OpenAI;
+using CereBro.Tools;
+using CereBro.Tools.Examples;
+
+namespace CereBro.Playground
+{
+    public static class Program
+    {
+        public static Task Main(string[] args)
+        {
+            return Runner.StartConversation<OpenAIChatProvider, ConsoleChatDispatcher>(new ITool[]
+            {
+                new LocationTool(),
+                new DateTool(),
+                new WeatherTool()
+            });
+        }
+    }
+}
---
+++ src/CereBro.Playground/README.md
@@ -0,0 +1,5 @@
+# CereBro.Playground
+
+```bash
+dotnet run --project src/CereBro.Playground/CereBro.Playground.csproj
+```
